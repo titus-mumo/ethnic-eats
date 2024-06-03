5113560aad2a7b7345a56c6ca8a74dc473bf3837
Titus Kennedy <tituskennedy74@gmail.com>
2024-06-03 20:40:32 +0300

    use gunicorn

--- ethnic_eats/ethnic_eats/asgi.py
+++ ethnic_eats/ethnic_eats/asgi.py
@@ -1,23 +1,26 @@
 import os
-
-from .wsgi import *
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+# from .wsgi import *
+# from channels.security.websocket import AllowedHostsOriginValidator
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ethnic_eats.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+
 django_asgi_app = get_asgi_application()
 
 from chatrooms.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
         ),
     }
 )
