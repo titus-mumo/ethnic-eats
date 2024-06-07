1769bc82ebf707a2c87bf4a9b258e9981cfdc168
Titus Kennedy <tituskennedy74@gmail.com>
2024-06-07 07:25:41 +0300

    Remove search app

--- ethnic_eats/ethnic_eats/settings.py
+++ ethnic_eats/ethnic_eats/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
     'chatrooms',
-    'search',
+    # 'search',
 ]
 
 MIDDLEWARE = [
--- ethnic_eats/search/routing.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from .consumers import CuisineConsumer
-
-websocket_urlpatterns = [
-    path('ws/cuisine/', CuisineConsumer.as_asgi()),
-]
