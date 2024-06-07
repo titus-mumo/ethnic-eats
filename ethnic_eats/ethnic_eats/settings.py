387762e69785d679036000859575057835acf9d6
Titus Kennedy <tituskennedy74@gmail.com>
2024-06-07 07:15:04 +0300

    Initiate a redis instance in a remote server

--- ethnic_eats/ethnic_eats/settings.py
+++ ethnic_eats/ethnic_eats/settings.py
@@ -162,7 +162,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [('34.16.129.197', 6379, 'ethnic-eats.onrender.com')],
         },
     },
 }
