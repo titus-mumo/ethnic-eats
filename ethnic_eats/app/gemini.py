ddd1aa41aecc47cac3a4c852e28fc9df8d6eb9d7
Titus Kennedy <tituskennedy74@gmail.com>
2024-06-11 23:15:56 +0300

    hide credential

--- ethnic_eats/app/gemini.py
+++ ethnic_eats/app/gemini.py
@@ -4,12 +4,18 @@ from django.http import JsonResponse
 import json
 import re
 
+from dotenv import load_dotenv
+
 import google.generativeai as genai
 from pathlib import Path
 import hashlib
 from datetime import datetime
 
-GEMINI_API_KEY = 'AIzaSyBswPaYh2wO7dQddLCuow6QWqee6cH2A_o'
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
 # Set up the model
@@ -21,31 +27,6 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest", generation_config=generation_config,)
-
-
-
-# class TrendingRestaurants(APIView):
-#     #permission_classes = [permissions.IsAuthenticated]
-
-#     def get(self, request = {}):
-#         response = model.generate_content("Examples of trending restaurants in the UK (output a list containing dicts of the restaurants and description ,like in programing). Note that i don't want the type, I want the specific restaurant name. Do not add any other content.")
-#         # Define pattern to extract restaurant names and descriptions
-#         pattern = re.compile(r'\{\s*"restaurant":\s*"([^"]+)",\s*"description":\s*"([^"]+)"\s*\}')
-
-#         # Find all matches in the response text
-#         matches = pattern.findall(response.text)
-
-#         # Create a list of dictionaries with the extracted data
-#         restaurants = [{'restaurant': match[0], 'description': match[1]} for match in matches]
-
-#         # Convert the list to JSON format
-#         json_response = json.dumps(restaurants, indent=2)
-
-#         # Clean up JSON response by removing unnecessary characters
-#         cleaned_response = json_response.replace("\\", "").replace("\n", "")
-
-#         # Return the cleaned response
-#         return JsonResponse(cleaned_response, status=status.HTTP_200_OK, safe=False)
 
 def update_foods() -> None:
         response = model.generate_content("Give a list of trending foods in the UK. Don't explain. Don't add any other information, at all.")
