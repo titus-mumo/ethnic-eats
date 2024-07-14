ab25099c13f78ebe4c5f6d6171b44e954f17be40
Titus Kennedy <tituskennedy74@gmail.com>
2024-07-15 01:05:07 +0300

    change redis configuration

--- ethnic_eats/ethnic_eats/settings.py
+++ ethnic_eats/ethnic_eats/settings.py
@@ -182,7 +182,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('redis', 6379)],
+            "hosts": [REDIS_URL],
             'capacity': 300
         },
     },
